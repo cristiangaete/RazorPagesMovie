709b64a2a829bc8daa2ee92ead56bdc846b3c816
Cristian-PC\Cristian <cristiangaetejordan@gmail.com>
2019-02-18 00:07:40 -0300

    Cambios

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -24,6 +24,12 @@ namespace RazorPagesMovie
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// El elemento RazorPagesMovieContext coordina la funcionalidad de EF Core (creación, lectura, actualización, eliminación, etc.)
+        /// para el modelo Movie. El contexto de datos (RazorPagesMovieContext) se deriva de 
+        /// Microsoft.EntityFrameworkCore.DbContext. En el contexto de datos se especifica qué entidades se incluyen en el modelo de datos.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
